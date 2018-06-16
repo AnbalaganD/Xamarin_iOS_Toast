9c8ffac41a6b122813eda5096e3531bd0ef2b66c
Anbalagan D <anbu94p@gmail.com>
2018-06-16 17:13:10 +0530

    clean up ViewController

--- Toast/ViewController.cs
+++ Toast/ViewController.cs
@@ -10,17 +10,5 @@ namespace Toast
         {
             // Note: this .ctor should not contain any initialization logic.
         }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
-        }
     }
 }
